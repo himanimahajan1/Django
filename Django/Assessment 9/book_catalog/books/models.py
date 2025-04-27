ffe1da45b64dfa4540f458d84d05836fc0e7876d
himanimahajan1 <136366185+himanimahajan1@users.noreply.github.com>
2025-04-28 00:12:32 +0530

    Add files via upload

---
+++ Django/Assessment 9/book_catalog/books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    description = models.TextField()
+    pages = models.IntegerField()
+    cover_image_url = models.URLField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
